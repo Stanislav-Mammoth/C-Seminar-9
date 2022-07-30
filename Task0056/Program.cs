28d5c7e591849d4881e1c5a310d8a6cf8571f0b3
Stanislav <mamontovsv@gmail.com>
2022-07-30 10:17:51 +0500

    Add homework

---
+++ Task0056/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Console.Write("Введите величину массива  m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите величину массива  n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] arr = new int [m,n];
+FillArray(arr);
+
+void FillArray(int [,] arr)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = new Random().Next(1,21); //[1,21)
+        Console.Write(arr[i,j]+ " ");     
+    }
+    Console.WriteLine();
+}
+}
+Console.WriteLine();
+
+//Нахождение строки с минимальной суммой
+int [] sum = new int[n];
+int min = 0;
+int minIndex = 0;
+for ( int i = 0; i < arr.GetLength(0); i++)
+{    
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+       sum[i] = sum[i] + arr[i,j];                              
+    }
+     if (sum[i] <= sum[0] ) 
+    {
+        min = sum[i];
+        minIndex = i;      
+    }     
+}
+
+//Вывод на экран
+Console.WriteLine("Строка с минимальной суммой");
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    Console.Write(arr[minIndex,j] + " "); 
+}
+
+        
+
+
+
+    
---
+++ Task0058/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Console.Write("Введите величину массива  m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите величину массива  n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] arr = new int [m,n];
+int [,] newarr = new int [m,n];
+FillArray(arr);
+FillArray(newarr);
+void FillArray(int [,] arr)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = new Random().Next(1,11); //[1,11)
+        Console.Write(arr[i,j]+ " ");     
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+}
+
+//Произведение матриц
+Console.WriteLine("Произведение двух матриц");
+int[,] multimatrix = new int[m,n];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int k = 0; k < arr.GetLength(0); k++)
+        {
+            multimatrix[i,j] = multimatrix[i,j] + arr[i,k]*newarr[k,j];
+        }
+        
+               
+        Console.Write(multimatrix[i,j]+ " ");     
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
